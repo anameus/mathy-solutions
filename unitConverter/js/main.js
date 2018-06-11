8727f95710b0360a78608cbd5474dd6328ce2f29
Anameus <annemie.burger@gmail.com>
2018-06-11 13:21:43 +0200

    creates correct dropdown by selecting temperature type. Need to still fix dropdown menu overwrite bug.

--- unitConverter/js/main.js
+++ unitConverter/js/main.js
@@ -5,8 +5,19 @@ $(document).ready(function(){
            //change/update button text with selected link's text:
           $(".btn:first-child").text($(this).text());
           $(".btn:first-child").val($(this).text());
-    
+           
        });
+        //populate the other dropdown menus:
+        $(".type-conv a").click(function(){
+          
+            if($(".type-conv a").text()==="Temperature"){
+               let newDropdownItem = $(".from-conv-item");
+                newDropdownItem.text("Degrees Celcius");
+                newDropdownItem.text("Farenheid"); //overwrites celcius..how to add items without overwriting old ones?
+               
+            }
+        });
+       
     
     });
    
