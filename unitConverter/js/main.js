4b0344eaefbeb1ed5db651a03bb089f368cb7a78
Anameus <annemie.burger@gmail.com>
2018-06-11 17:31:50 +0200

    add JSON file (data), add full qjuery file. Add Switch statement for unit dropdown list population function. Add forEach loop to generate dropdown items. Need to fix 404 error

--- unitConverter/js/main.js
+++ unitConverter/js/main.js
@@ -9,13 +9,34 @@ $(document).ready(function(){
        });
         //populate the other dropdown menus:
         $(".type-conv a").click(function(){
-          
-            if($(".type-conv a").text()==="Temperature"){
-               let newDropdownItem = $(".from-conv-item");
-                newDropdownItem.text("Degrees Celcius");
-                newDropdownItem.text("Farenheid"); //overwrites celcius..how to add items without overwriting old ones?
-               
-            }
+            let $dropdown = $(this);
+            console.log($dropdown);
+
+            $.getJSON("data.json", function(data){
+                let convType = $dropdown.val();
+                let unitsOfType = [];
+
+                switch(convType){
+                    case 'Temperature':
+                        unitsOfType = data.Temperature.split(",");
+                        break;
+                    case 'Currency':
+                        unitsOfType = data.Currency.split(",");
+                        break;
+                    case 'Length':
+                        unitsOfType = data.Length.split(",");
+                        break;
+                    default:
+                        unitsOfType = ['Please choose type of conversion'];
+                }
+
+                let $fromConv = $(".from-conv");
+                $fromConv.empty();
+                $.each(unitsOfType, function(index,value){
+                    $fromConv.append("<a class='dropdown-item from-conv-item' href='#'>"+value+"</a>");
+                });
+            })
+           
         });
        
     
